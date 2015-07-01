ac21bafb3719de112b0a977b8d4290f1a2eb8bc3
tarena <tarena@babadeiMac.local>
2015-07-01 12:01:52 +0800

    aaa

---
+++ fucj/fucj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fucj
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fucj/fucj/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fucj
+//
+//  Created by tarena on 15/7/1.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"1");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
